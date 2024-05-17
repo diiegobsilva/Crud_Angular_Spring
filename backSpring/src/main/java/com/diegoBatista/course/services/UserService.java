dfa90097f2aee766fae4292233d967c414e7383d
Diego Silva <diegosilva157890@gmail.com>
2024-05-17 16:29:04 -0300

    Service layer, component registration

---
+++ backSpring/src/main/java/com/diegoBatista/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.diegoBatista.course.services;
+
+import com.diegoBatista.course.entities.User;
+import com.diegoBatista.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
